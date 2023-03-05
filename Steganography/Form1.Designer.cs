46173567866731c23c67fe5e22a0310c39d49e9f
Ekaterina <Eamaltseva14@gmail.com>
2023-03-05 15:26:28 +0400

    Fircn commit

---
+++ Steganography/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿
+namespace Steganography
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.src = new System.Windows.Forms.TextBox();
+            this.download = new System.Windows.Forms.Button();
+            this.save = new System.Windows.Forms.Button();
+            this.picture1 = new System.Windows.Forms.PictureBox();
+            this.picture2 = new System.Windows.Forms.PictureBox();
+            this.text1 = new System.Windows.Forms.TextBox();
+            this.text2 = new System.Windows.Forms.TextBox();
+            this.coding = new System.Windows.Forms.Button();
+            this.decoding = new System.Windows.Forms.Button();
+            this.downloadDec = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picture1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // src
+            // 
+            this.src.Location = new System.Drawing.Point(16, 12);
+            this.src.Name = "src";
+            this.src.Size = new System.Drawing.Size(532, 22);
+            this.src.TabIndex = 0;
+            // 
+            // download
+            // 
+            this.download.Location = new System.Drawing.Point(554, 8);
+            this.download.Name = "download";
+            this.download.Size = new System.Drawing.Size(121, 31);
+            this.download.TabIndex = 1;
+            this.download.Text = "Загрузить";
+            this.download.UseVisualStyleBackColor = true;
+            this.download.Click += new System.EventHandler(this.download_Click);
+            // 
+            // save
+            // 
+            this.save.Location = new System.Drawing.Point(1215, 8);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(121, 31);
+            this.save.TabIndex = 2;
+            this.save.Text = "Сохранить";
+            this.save.UseVisualStyleBackColor = true;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // picture1
+            // 
+            this.picture1.Location = new System.Drawing.Point(16, 45);
+            this.picture1.Name = "picture1";
+            this.picture1.Size = new System.Drawing.Size(657, 420);
+            this.picture1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picture1.TabIndex = 3;
+            this.picture1.TabStop = false;
+            // 
+            // picture2
+            // 
+            this.picture2.Location = new System.Drawing.Point(679, 45);
+            this.picture2.Name = "picture2";
+            this.picture2.Size = new System.Drawing.Size(657, 420);
+            this.picture2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picture2.TabIndex = 5;
+            this.picture2.TabStop = false;
+            // 
+            // text1
+            // 
+            this.text1.Location = new System.Drawing.Point(16, 475);
+            this.text1.Name = "text1";
+            this.text1.Size = new System.Drawing.Size(486, 22);
+            this.text1.TabIndex = 6;
+            // 
+            // text2
+            // 
+            this.text2.Location = new System.Drawing.Point(679, 475);
+            this.text2.Name = "text2";
+            this.text2.Size = new System.Drawing.Size(486, 22);
+            this.text2.TabIndex = 7;
+            // 
+            // coding
+            // 
+            this.coding.Location = new System.Drawing.Point(508, 471);
+            this.coding.Name = "coding";
+            this.coding.Size = new System.Drawing.Size(121, 31);
+            this.coding.TabIndex = 8;
+            this.coding.Text = "Зашифровать";
+            this.coding.UseVisualStyleBackColor = true;
+            this.coding.Click += new System.EventHandler(this.coding_Click);
+            // 
+            // decoding
+            // 
+            this.decoding.Location = new System.Drawing.Point(1171, 470);
+            this.decoding.Name = "decoding";
+            this.decoding.Size = new System.Drawing.Size(121, 31);
+            this.decoding.TabIndex = 9;
+            this.decoding.Text = "Расшифровать";
+            this.decoding.UseVisualStyleBackColor = true;
+            this.decoding.Click += new System.EventHandler(this.decoding_Click);
+            // 
+            // downloadDec
+            // 
+            this.downloadDec.Location = new System.Drawing.Point(681, 8);
+            this.downloadDec.Name = "downloadDec";
+            this.downloadDec.Size = new System.Drawing.Size(231, 31);
+            this.downloadDec.TabIndex = 11;
+            this.downloadDec.Text = "Загрузить для расшифрования";
+            this.downloadDec.UseVisualStyleBackColor = true;
+            this.downloadDec.Click += new System.EventHandler(this.downloadDec_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1355, 513);
+            this.Controls.Add(this.downloadDec);
+            this.Controls.Add(this.decoding);
+            this.Controls.Add(this.coding);
+            this.Controls.Add(this.text2);
+            this.Controls.Add(this.text1);
+            this.Controls.Add(this.picture2);
+            this.Controls.Add(this.picture1);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.download);
+            this.Controls.Add(this.src);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picture1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox src;
+        private System.Windows.Forms.Button download;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.PictureBox picture1;
+        private System.Windows.Forms.PictureBox picture2;
+        private System.Windows.Forms.TextBox text1;
+        private System.Windows.Forms.TextBox text2;
+        private System.Windows.Forms.Button coding;
+        private System.Windows.Forms.Button decoding;
+        private System.Windows.Forms.Button downloadDec;
+    }
+}
+
---
+++ Steganography/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Steganography
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void download_Click(object sender, EventArgs e)
+        {
+            picture2.Image = null;
+            if (src.Text.Equals(""))
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "Files (*.jpg, *.png)|*.jpg; *.png|Files (*.jpg)|*.jpg|Files (*.png)|*.png|All files (*.*)|*.*";
+                openFileDialog.RestoreDirectory = true;
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    src.Text = openFileDialog.FileName;
+                }
+            }
+            if (!src.Text.Equals(""))
+            {
+                picture1.Image = Image.FromFile(src.Text);
+            }
+        }
+
+        private void downloadDec_Click(object sender, EventArgs e)
+        {
+            picture1.Image = null;
+            if (src.Text.Equals(""))
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "Files (*.jpg, *.png)|*.jpg; *.png|Files (*.jpg)|*.jpg|Files (*.png)|*.png|All files (*.*)|*.*";
+                openFileDialog.RestoreDirectory = true;
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    src.Text = openFileDialog.FileName;
+                }
+            }
+            if (!src.Text.Equals(""))
+            {
+                picture2.Image = Image.FromFile(src.Text);
+            }
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            if (picture2.Image != null) //если в pictureBox есть изображение
+            {
+                //создание диалогового окна "Сохранить как..", для сохранения изображения
+                SaveFileDialog savedialog = new SaveFileDialog();
+                savedialog.InitialDirectory = "c:\\";
+                savedialog.Title = "Сохранить картинку как...";
+                //отображать ли предупреждение, если пользователь указывает имя уже существующего файла
+                savedialog.OverwritePrompt = true;
+                //отображать ли предупреждение, если пользователь указывает несуществующий путь
+                savedialog.CheckPathExists = true;
+                //список форматов файла, отображаемый в поле "Тип файла"
+                savedialog.Filter = "Image Files(*.PNG)|*.PNG|All files (*.*)|*.*";
+                //отображается ли кнопка "Справка" в диалоговом окне
+                savedialog.ShowHelp = true;
+                savedialog.FileName = "Picture.png";
+                if (savedialog.ShowDialog() == DialogResult.OK) //если в диалоговом окне нажата кнопка "ОК"
+                {
+                    try
+                    {
+                        picture2.Image.Save(savedialog.FileName, System.Drawing.Imaging.ImageFormat.Png);
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Невозможно сохранить изображение", "Ошибка",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        private void coding_Click(object sender, EventArgs e)
+        {
+            if(picture1.Image == null) // Если изображение не было загружено
+            {
+                var location = Application.StartupPath;
+                var path = Path.GetDirectoryName(location);
+                picture1.Image = Image.FromFile(Path.GetDirectoryName(path) + "\\img\\ph.jpg"); // Загрузка стандартного изображения
+            }
+
+            Image img = picture1.Image;
+            Bitmap bitmap = new Bitmap(img);
+
+            int nBytes = (byte)text1.Text.Length * 2; // Кол-во байт в тексте
+            byte[] bytesText = new byte[nBytes + 1]; // Создание масства байт
+            bytesText[0] = (byte)nBytes; // 1 байт - длина кодируемого текста 
+            Encoding.UTF8.GetBytes(text1.Text, 0, text1.Text.Length, bytesText, 1); // Кодирует набор символов из заданного объекта в указанный массив байтов
+            //(Объект, Индекс первого кодируемого символа, Число кодируемых символов, Массив байтовИндекс, с которого начинается запись)
+
+            int positionBytes = 0;
+            for (int i = 0; i < bitmap.Width; i++)
+            {
+                for(int j = 0; j < bitmap.Height; j++)
+                {
+                    if (bytesText.Length == positionBytes) // При достижении конца текста, вывод получившийся картинки
+                    {
+                        picture2.Image = Image.FromHbitmap(bitmap.GetHbitmap());
+                        return;                        
+                    }
+
+                    Color pixelColor = bitmap.GetPixel(i, j); // Получение цвета пикселя
+
+                    byte r = (byte)((pixelColor.R & 248) | (bytesText[positionBytes] >> 0) & 7); // Изменение 3 младших значимых битов в цвете R на 3 бита кодируемого текста
+                    byte g = (byte)((pixelColor.G & 248) | (bytesText[positionBytes] >> 3) & 7); // Изменение 3 младших значимых битов в цвете G на 3 бита кодируемого текста
+                    byte b = (byte)((pixelColor.B & 252) | (bytesText[positionBytes] >> 6) & 3); // Изменение 2 младших значимых битов в цвете B на 2 бита кодируемого текста
+                    positionBytes++;
+
+                    bitmap.SetPixel(i, j, Color.FromArgb(r, g, b)); // Замена оригинальных пикселей на изменённые
+                }
+            }
+            MessageBox.Show("Длина строки слишком большая");
+        }
+
+        private void decoding_Click(object sender, EventArgs e)
+        {
+            if (picture2.Image != null) // Если изображение загружено
+            {
+                Image img = picture2.Image; // Получение изображение с зашифрованным текстом
+                Bitmap bitmap = new Bitmap(img);
+
+                Color pixel1 = bitmap.GetPixel(0, 0); // Получение первого пикселя, в котором зашифрована длина текста
+                int length = (int)(((pixel1.B & 3) << 6) | ((pixel1.G & 7) << 3)| ((pixel1.R & 7) << 0)); // Получение длины
+
+                byte[] bytesText = new byte[length]; // Создание массива битов для текста
+                
+                int positionBytes = 0; // Положение в тексе
+                for (int i = 0; i < bitmap.Width; i++)
+                {
+                    for (int j = 1; j < bitmap.Height; j++)
+                    {
+                        if (bytesText.Length == positionBytes)
+                        {
+                            text2.Text = Encoding.UTF8.GetString(bytesText, 0, length); // Вывод полученного текста
+                            return;
+                        }                           
+
+                        Color clrPixelModified = bitmap.GetPixel(i, j);
+                        byte bitsR = (byte)((clrPixelModified.R & 7) << 0); // Получение бит из красного цвета
+                        byte bitsG = (byte)((clrPixelModified.G & 7) << 3); // Получение бит из зелёного цвета
+                        byte bitsB = (byte)((clrPixelModified.B & 3) << 6); // Получение бит из синего цвета
+
+                        bytesText[positionBytes] = (byte)(bitsR | bitsG | bitsB); // Объединение бит в байт
+                        positionBytes++;
+                    }
+                }
+            }            
+        }
+    }
+}
